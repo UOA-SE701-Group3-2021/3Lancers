d01bc637be6d69e7438ec7361314649f2649f11e
Carrie <cng822@aucklanduni.ac.nz>
2021-03-17 03:36:58 +1300

    implemented design todo list

--- telos-frontend/src/components/todo/JournalTodo.jsx
+++ telos-frontend/src/components/todo/JournalTodo.jsx
@@ -1,3 +1,204 @@
-const JournalTodo = () => <div />;
+/* eslint-disable no-unused-vars */
+import {
+  Checkbox,
+  Divider,
+  Box,
+  FormControl,
+  OutlinedInput,
+  InputLabel,
+  InputAdornment,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  TextField,
+  Select,
+  Menu,
+  MenuItem,
+} from '@material-ui/core';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
+import { useState } from 'react';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import './JournalTodo.css';
+import ErrorIcon from '@material-ui/icons/Error';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    color: 'rgba(0, 0, 0, 0.6)',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+const listitems = [
+  {
+    name: 'OnGoing',
+    onGoing: true,
+  },
+  //   {
+  //     name: 'OutDated',
+  //     onGoing: false,
+  //   },
+];
+
+const outdated = {
+  color: '#FF0000',
+};
+
+const options = ['Cancel', 'Reschedule'];
+
+const JournalTodo = () => {
+  const classes = useStyles();
+  const [checked, setChecked] = useState([0]);
+  const [item, setItem] = useState('');
+  const [newItem, setNewItem] = useState([]);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+    setChecked(newChecked);
+  };
+
+  const firstEvent = (event) => {
+    setItem(event.target.value);
+  };
+
+  const secondEvent = () => {
+    setNewItem((prev) => [...prev, item]);
+    setItem('');
+  };
+
+  return (
+    <Box className="container">
+      <div>
+        <p className="title"> To Do </p>
+      </div>
+      <Divider />
+      <List className={classes.root}>
+        {newItem.map((value) => {
+          const labelId = `checkbox-list-label-${value}`;
+
+          return (
+            <ListItem
+              className="listItem"
+              key={value}
+              role={undefined}
+              dense
+              button
+              onClick={handleToggle(value)}
+            >
+              {value.onGoing ? (
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    style={{ color: '#FF0000' }}
+                    checked={checked.indexOf(value) !== -1}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ 'aria-labelledby': labelId }}
+                  />
+                </ListItemIcon>
+              ) : (
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    style={{ color: '#6200EE' }}
+                    checked={checked.indexOf(value) !== -1}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ 'aria-labelledby': labelId }}
+                  />
+                </ListItemIcon>
+              )}
+              {value.onGoing ? (
+                <ListItemText
+                  primaryTypographyProps={{ style: outdated }}
+                  id={labelId}
+                  primary={` ${value}`}
+                />
+              ) : (
+                <ListItemText id={labelId} primary={` ${value}`} />
+              )}
+              {value.onGoing ? (
+                <ListItemSecondaryAction>
+                  <IconButton
+                    edge="end"
+                    aria-controls="simple-menu"
+                    aria-haspopup="true"
+                    onClick={handleClick}
+                  >
+                    <ErrorIcon style={{ color: '#EB5757' }} />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              ) : (
+                <ListItemSecondaryAction>
+                  <IconButton
+                    edge="end"
+                    aria-controls="simple-menu"
+                    aria-haspopup="true"
+                    onClick={handleClick}
+                  >
+                    <MoreVertIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              )}
+            </ListItem>
+          );
+        })}
+      </List>
+      <div>
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">New To Do</InputLabel>
+          <OutlinedInput
+            id="outlined-todo"
+            value={item}
+            onChange={firstEvent}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton className="AddBtn" onClick={secondEvent}>
+                  <AddIcon className="Publish" />
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={70}
+          />
+        </FormControl>
+      </div>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem className="menubar" onClick={handleClose}>
+          Cancel
+        </MenuItem>
+        <MenuItem className="menubar" onClick={handleClose}>
+          Reschedule
+        </MenuItem>
+      </Menu>
+    </Box>
+  );
+};
 
 export default JournalTodo;
--- telos-frontend/src/index.js
+++ telos-frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "fontsource-roboto";
+import JournalTodo from './components/todo/JournalTodo';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <JournalTodo />
   </React.StrictMode>,
   document.getElementById('root')
 );
