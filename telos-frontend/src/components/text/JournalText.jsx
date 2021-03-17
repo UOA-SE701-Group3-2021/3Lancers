5f7cc0b62705ddef2385ce4a45ae76c9e173d43d
JChamberlain28 <51986824+JChamberlain28@users.noreply.github.com>
2021-03-18 12:29:20 +1300

    Closes #45: Design of text widget (journal / daily view) implemented (#107)
    * created basic textarea react component that fills the size of its parent
    
    * implemented textarea and background Material UI paper that forms widget
    
    * fixed positioning, restrucutred component to fit convention

--- telos-frontend/src/components/text/JournalText.jsx
+++ telos-frontend/src/components/text/JournalText.jsx
@@ -1,3 +1,11 @@
-const JournalText = () => <div />;
+import Paper from '@material-ui/core/Paper';
+import styles from './TextWidget.module.css';
+
+// This react component draws a Material-UI paper background and overlays a text area on it for the user to make notes
+const JournalText = () => (
+  <Paper className={styles.paper} variant="outlined">
+    <textarea className={styles.textInput} />
+  </Paper>
+);
 
 export default JournalText;
