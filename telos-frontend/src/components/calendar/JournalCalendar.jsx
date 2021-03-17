86e619509112a5a603870da201e98ac663f0552f
maheknangia <63040474+maheknangia@users.noreply.github.com>
2021-03-17 23:14:21 +1300

    Basic timeline of events working

--- telos-frontend/src/components/calendar/JournalCalendar.jsx
+++ telos-frontend/src/components/calendar/JournalCalendar.jsx
@@ -1,58 +1,98 @@
 // import {useState, useEffect} from "react";
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
-import * as ReactBootStrap from "react-bootstrap"
+// import * as ReactBootStrap from 'react-bootstrap';
+// import ReactDOM from 'react-dom';
+import Timeline from '@material-ui/lab/Timeline';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+import Typography from '@material-ui/core/Typography';
 
-let times;
-const JournalCalendar = () => {
-    times = [
-        {time: "12:00 AM", activity: ""},
-        {time: "1:00 AM", activity: "Sleep"},
-        {time: "2:00 AM", activity: ""},
-        {time: "3:00 AM", activity: ""},
-        {time: "4:00 AM", activity: ""},
-        {time: "5:00 AM", activity: ""},
-        {time: "6:00 AM", activity: "Wake up"},
-        {time: "7:00 AM", activity: "Coffee"},
-        {time: "8:00 AM", activity: "Assignment"},
-        {time: "9:00 AM", activity: ""},
-        {time: "10:00 AM", activity: "Food"},
-        {time: "11:00 AM", activity: "Gym"},
-        {time: "12:00 PM", activity: ""},
-        {time: "1:00 PM", activity: "Sleep"},
-        {time: "2:00 PM", activity: ""},
-        {time: "3:00 PM", activity: ""},
-        {time: "4:00 PM", activity: ""},
-        {time: "5:00 PM", activity: ""},
-        {time: "6:00 PM", activity: "Wake up"},
-        {time: "7:00 PM", activity: "Coffee"},
-        {time: "8:00 PM", activity: "Assignment"},
-        {time: "9:00 PM", activity: ""},
-        {time: "10:00 PM", activity: "Food"},
-        {time: "11:00 PM", activity: "Gym"},
-    ]
-    // eslint-disable-next-line no-shadow
-    const renderTable = (times, index) => (
-        <table style={{"borderWidth":"3px", 'borderColor':"#aaaaaa", 'borderStyle':'solid'}}>
-            <tr key={index}>
-                <td>{times.time}</td>
-                <td>{times.activity}</td>
-            </tr>
-        </table>
-
-        )
-return(
-        <div>
-            <ReactBootStrap.Table striped bordered hover size="sm">
-                <thead>
-                <tr>
-                    <th>Calendar</th>
-                </tr>
-                </thead>
-                <tbody>
-                {times.map(renderTable)}
-                </tbody>
-            </ReactBootStrap.Table>
-        </div>
-);
+export default function BasicTimeline() {
+  return (
+    <Timeline align="left">
+      <h1>Calendar</h1>
+      <TimelineItem>
+        <Typography variant="body1" color="textSecondary">
+          7:00 am
+        </Typography>
+        <TimelineSeparator>
+          <TimelineDot color="primary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Wake Up</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <Typography variant="body1" color="textSecondary">
+          8:00 am
+        </Typography>
+        <TimelineSeparator>
+          <TimelineDot color="primary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Have Breakfast</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <Typography variant="body1" color="textSecondary">
+          9:00 am
+        </Typography>
+        <TimelineSeparator>
+          <TimelineDot color="primary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Go to University</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <Typography variant="body1" color="textSecondary">
+          12:00 pm
+        </Typography>
+        <TimelineSeparator>
+          <TimelineDot color="primary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Have Lunch</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <Typography variant="body1" color="textSecondary">
+          1:00 pm
+        </Typography>
+        <TimelineSeparator>
+          <TimelineDot color="primary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Attend Lectures</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <Typography variant="body1" color="textSecondary">
+          6:00 pm
+        </Typography>
+        <TimelineSeparator>
+          <TimelineDot color="primary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Go Home</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <Typography variant="body1" color="textSecondary">
+          7:00 pm
+        </Typography>
+        <TimelineSeparator>
+          <TimelineDot color="primary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Do Uni Work</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <Typography variant="body1" color="textSecondary">
+          11:00 pm
+        </Typography>
+        <TimelineSeparator>
+          <TimelineDot color="primary" />
+        </TimelineSeparator>
+        <TimelineContent>Sleep</TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  );
 }
-export default JournalCalendar;
