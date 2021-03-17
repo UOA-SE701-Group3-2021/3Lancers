2b68a34236ddd9e9927341fb072634efa6c5a55c
Yicheng Sun <lhsunyc@outlook.com>
2021-03-17 13:36:12 +0800

    Update index.js

--- telos-frontend/src/index.js
+++ telos-frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import DashboardHabitTracker from './components/habit-tracker/DashboardHabitTracker';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'fontsource-roboto';
 
 ReactDOM.render(
   <React.StrictMode>
-    <DashboardHabitTracker />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
