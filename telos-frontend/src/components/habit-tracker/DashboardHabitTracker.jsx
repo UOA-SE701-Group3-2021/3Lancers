d6ad2052a55825730ef1473695b1b4f312362ec9
lhsunyc <53070298+lhsunyc@users.noreply.github.com>
2021-03-17 16:13:24 +0800

    Implemented dashboard view of habit tracker (#106)
    * Complete implementation of design
    
    * Update index.js
    
    * Reset index.js

--- telos-frontend/src/components/habit-tracker/DashboardHabitTracker.jsx
+++ telos-frontend/src/components/habit-tracker/DashboardHabitTracker.jsx
@@ -1,3 +1,32 @@
-const DashboardHabitTracker = () => <div />;
+import AddIcon from '@material-ui/icons/Add';
+import IconButton from '@material-ui/core/IconButton';
+import './DashboardHabitTracker.css';
+
+const DashboardHabitTracker = () => (
+  <div className="container">
+    <div className="HabitTitle">
+      <h2>hello world</h2>
+      <span className="AddIcon">
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+      </span>
+    </div>
+    <div className="HabitContent">
+      <p>Habit description, Do Task X, Y many times per week</p>
+    </div>
+    <div className="WeekSchedule">
+      <ul>
+        <li>M</li>
+        <li>T</li>
+        <li>W</li>
+        <li>T</li>
+        <li>F</li>
+        <li>S</li>
+        <li>S</li>
+      </ul>
+    </div>
+  </div>
+);
 
 export default DashboardHabitTracker;
