80be8e8d0a1af30ed5a89275d1cabdd0241bc985
Laura Huijser <54383847+lhuijser@users.noreply.github.com>
2021-03-18 11:56:05 +1300

    Implement basic Journal design (#111)
    * #61 - Implement basic journal design
    
    * Fix PR comment change suggestions - linting and test

--- telos-frontend/src/views/journal/Journal.jsx
+++ telos-frontend/src/views/journal/Journal.jsx
@@ -1,3 +1,15 @@
-const Journal = () => <div />;
+import './Journal.css';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import Page from './Page';
+
+const Journal = () => (
+  <div className="Journal">
+    <ArrowBackIcon className="Arrow-left" />
+    <Page />
+    <Page />
+    <ArrowForwardIcon className="Arrow-right" />
+  </div>
+);
 
 export default Journal;
