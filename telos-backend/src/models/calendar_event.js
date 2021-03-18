52d19a9c18552ef9ee18d6bea694a9f33972d586
flexzy <fzyang2000@hotmail.co.nz>
2021-03-18 17:21:23 +1300

    Added calendar events schema V1

---
+++ telos-backend/src/models/calendar_event.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const calendarEventSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        startTime: {
+            type: Date,
+            required: true,
+        },
+        endTime: {
+            type: Date,
+            required: true,
+        },
+    },
+);
+
+module.exports = mongoose.model('CalendarEvent', calendarEventSchema);
