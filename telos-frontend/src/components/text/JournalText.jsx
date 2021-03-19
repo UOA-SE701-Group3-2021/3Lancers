ea16418bfe97edb2b374f0f840b79886182a8688
JChamberlain28 <JChamberlain28@outlook.com>
2021-03-20 12:11:04 +1300

    ran prittier again on files it didnt apply to

--- telos-frontend/src/components/text/JournalText.jsx
+++ telos-frontend/src/components/text/JournalText.jsx
@@ -4,21 +4,23 @@ import styles from './TextWidget.module.css';
 
 // This react component draws a Material-UI paper background and overlays a text area on it for the user to make notes
 const JournalText = () => {
-  
   // text widgeds content stored in react state
-  const [textContent, setTextContent] = useState('')
+  const [textContent, setTextContent] = useState('');
 
   function onTextChange(text) {
     setTextContent(text);
-    
-  } 
-
+  }
 
   return (
-  <Paper className={styles.paper} variant="outlined">
-    <textarea data-testid="textInput" className={styles.textInput} value={textContent} onChange={e => onTextChange(e.target.value)} />
-  </Paper>
+    <Paper className={styles.paper} variant="outlined">
+      <textarea
+        data-testid="textInput"
+        className={styles.textInput}
+        value={textContent}
+        onChange={(e) => onTextChange(e.target.value)}
+      />
+    </Paper>
   );
-  }
+};
 
 export default JournalText;
