701b7f4ea36bf126f83ce5421e7c9cf175843980
Milk <50122790+Milk-Yan@users.noreply.github.com>
2021-03-19 19:13:54 +1300

    Added routing for all the different components with url links. (#132)

--- telos-frontend/src/App.js
+++ telos-frontend/src/App.js
@@ -1,13 +1,48 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Journal from './views/journal/Journal';
+import CalendarDashboard from './components/calendar/DashboardCalendar';
+import CalendarJournal from './components/calendar/JournalCalendar';
+import DashboardHabitTracker from './components/habit-tracker/DashboardHabitTracker';
+import JournalHabitTracker from './components/habit-tracker/JournalHabitTracker';
+import DashboardTodo from './components/todo/DashboardTodo';
+import JournalTodo from './components/todo/JournalTodo';
+import TopBar from './components/top-bar/TopBar';
 
 function App() {
   return (
-    <div className="App" data-testid="test">
-      <div className="Journal-view">
-        <Journal />
-      </div>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/test/dashboard/calendar">
+          <CalendarDashboard />
+        </Route>
+        <Route path="/test/journal/calendar">
+          <CalendarJournal />
+        </Route>
+        <Route path="/test/dashboard/habittracker">
+          <DashboardHabitTracker />
+        </Route>
+        <Route path="/test/journal/habittracker">
+          <JournalHabitTracker />
+        </Route>
+        <Route path="/test/dashboard/todo">
+          <DashboardTodo />
+        </Route>
+        <Route path="/test/journal/todo">
+          <JournalTodo />
+        </Route>
+        <Route path="/test/topbar">
+          <TopBar />
+        </Route>
+        <Route path="*">
+          <div className="App" data-testid="test">
+            <div className="Journal-view">
+              <Journal />
+            </div>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
