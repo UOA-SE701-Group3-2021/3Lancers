382396e7bd111dbfbaddf4917cb3b90c13f56d99
JChamberlain28 <JChamberlain28@outlook.com>
2021-03-19 20:18:00 +1300

    removed debugging print statment and added comment on text state

--- telos-frontend/src/components/text/JournalText.jsx
+++ telos-frontend/src/components/text/JournalText.jsx
@@ -5,10 +5,10 @@ import styles from './TextWidget.module.css';
 // This react component draws a Material-UI paper background and overlays a text area on it for the user to make notes
 const JournalText = () => {
   
+  // text widgeds content stored in react state
   const [textContent, setTextContent] = useState('')
 
   function onTextChange(text) {
-    console.log(text)
     setTextContent(text);
   }
 
