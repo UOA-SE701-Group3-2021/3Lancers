6f310539a638ac5159d1c7a48b9ac756a19fa79f
JChamberlain28 <51986824+JChamberlain28@users.noreply.github.com>
2021-03-20 12:38:27 +1300

    Feature text widget state functionality (#136)
    * implemented basic useState to hold current text content
    
    * merged master in
    
    * removed text widged from app, to prep for merge
    
    * removed debugging print statment and added comment on text state
    
    * created text widget test class with empty test, added react-test-renderer dependency
    
    * implemented test to check state change of text in Journal Text widget
    
    * ran prittier
    
    * ran prittier again on files it didnt apply to

--- telos-frontend/src/components/text/JournalText.jsx
+++ telos-frontend/src/components/text/JournalText.jsx
@@ -1,11 +1,26 @@
 import Paper from '@material-ui/core/Paper';
+import { useState } from 'react';
 import styles from './TextWidget.module.css';
 
 // This react component draws a Material-UI paper background and overlays a text area on it for the user to make notes
-const JournalText = () => (
-  <Paper className={styles.paper} variant="outlined">
-    <textarea className={styles.textInput} />
-  </Paper>
-);
+const JournalText = () => {
+  // text widgeds content stored in react state
+  const [textContent, setTextContent] = useState('');
+
+  function onTextChange(text) {
+    setTextContent(text);
+  }
+
+  return (
+    <Paper className={styles.paper} variant="outlined">
+      <textarea
+        data-testid="textInput"
+        className={styles.textInput}
+        value={textContent}
+        onChange={(e) => onTextChange(e.target.value)}
+      />
+    </Paper>
+  );
+};
 
 export default JournalText;
