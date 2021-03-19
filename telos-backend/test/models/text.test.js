a9aa1d6461ec8898627e0238581ae78484260ee7
flexzy <fzyang2000@hotmail.co.nz>
2021-03-19 17:48:49 +1300

    Created tests for the text model definition

---
+++ telos-backend/test/models/text.test.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-await-in-loop, no-return-await */
+const mongoose = require('mongoose');
+const dbHelper = require('../db-helper');
+// Require the models from src/models
+const text = require('../../src/models/text');
+
+beforeAll(async () => await dbHelper.connectToDb());
+
+afterEach(async () => await dbHelper.clearDb());
+
+afterAll(async () => await dbHelper.closeDb());
+
+describe('Test Text Model', () => {
+  it('can be created and saved in memory', async () => {
+    const testTextData = {
+      text: 'lorem ipsum',
+      widgetId: '5d6ede6a0ba62570afcedd3a',
+    };
+    const validTestData = new text(testTextData);
+    const savedTestModel = await validTestData.save();
+
+    expect(savedTestModel.text).toBe(validTestData.text);
+    expect(savedTestModel.widgetId).toBe(validTestData.widgetId);
+
+    expect(savedTestModel._id).toBeDefined();
+  });
+
+  it('can validate improper widgetId inputs', async () => {
+    const testTextData = {
+      text: 'lorem ipsum',
+      widgetId: 'fakeid',
+    };
+    const invalidTestData = new text(testTextData);
+    let err;
+    try {
+      await invalidTestData.save();
+    } catch(error) {
+      err = error;
+    }
+
+    expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+  });
+});
+
+
