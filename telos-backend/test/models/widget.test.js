4e52cf00ce00dcafd6822301d264dbdb731aaee3
flexzy <fzyang2000@hotmail.co.nz>
2021-03-19 18:13:14 +1300

    Created tests for the widget model definition

---
+++ telos-backend/test/models/widget.test.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-await-in-loop, no-return-await */
+const mongoose = require('mongoose');
+const dbHelper = require('../db-helper');
+// Require the models from src/models
+const Widget = require('../../src/models/widget');
+
+beforeAll(async () => await dbHelper.connectToDb());
+
+afterEach(async () => await dbHelper.clearDb());
+
+afterAll(async () => await dbHelper.closeDb());
+
+describe('Test Widget Model', () => {
+  it('can be created and saved in memory', async () => {
+    const testWidgetData = {
+      date: '2021-03-19',
+      position: {
+        row: 3,
+        col: 2,
+      },
+      type: 'calendar',
+    };
+    const validTestData = new Widget(testWidgetData);
+    const savedTestModel = await validTestData.save();
+
+    expect(savedTestModel.date).toBe(validTestData.date);
+    expect(savedTestModel.position).toBe(validTestData.position);
+    expect(savedTestModel.type).toBe(validTestData.type);
+
+    expect(savedTestModel._id).toBeDefined();
+  });
+
+  it('can validate invalid widget types', async () => {
+    const testWidgetData = {
+      date: '2021-03-19',
+      position: {
+        row: 3,
+        col: 2,
+      },
+      type: 'fake widget',
+    };
+    const invalidTestData = new Widget(testWidgetData);
+    let err;
+    try {
+      await invalidTestData.save();
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+  });
+});
