5fd70fd82d74de9c784fa4e93a34890445b732c8
Laura Huijser <54383847+lhuijser@users.noreply.github.com>
2021-03-20 23:52:50 +1300

    Implemented components and functionality to change the current day (#152)
    * Implement basic journal design with new datepicker
    
    * Issue #64 - Added datepicker in place of the date text on the journal pages to choose date
    
    * Issue #64 - Sync the two journal pages when date is changed via the datepicker
    
    * Issue #64 - Styled the dates that appear on each journal page
    
    * Issue #64 Fixed node dependency issue

--- telos-frontend/src/views/journal/Journal.jsx
+++ telos-frontend/src/views/journal/Journal.jsx
@@ -1,15 +1,64 @@
+import { useState } from 'react';
 import './Journal.css';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import DatePicker from 'react-datepicker';
 import Page from './Page';
+import 'react-datepicker/dist/react-datepicker.css';
 
-const Journal = () => (
-  <div className="Journal">
-    <ArrowBackIcon className="Arrow-left" />
-    <Page />
-    <Page />
-    <ArrowForwardIcon className="Arrow-right" />
-  </div>
-);
+const Journal = () => {
+  const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+  const DAYS_TO_CHANGE_BY = 1;
+  const date = new Date();
+  const [dateLeftPage, setDateLeftPage] = useState(date.getTime());
+  const [dateRightPage, setDateRightPage] = useState(
+    date.getTime() + DAYS_TO_CHANGE_BY * MILLISECONDS_PER_DAY
+  );
+
+  function handleLeftNav() {
+    setDateLeftPage(dateLeftPage - DAYS_TO_CHANGE_BY * MILLISECONDS_PER_DAY);
+    setDateRightPage(dateRightPage - DAYS_TO_CHANGE_BY * MILLISECONDS_PER_DAY);
+  }
+
+  function handleRightNav() {
+    setDateLeftPage(dateLeftPage + DAYS_TO_CHANGE_BY * MILLISECONDS_PER_DAY);
+    setDateRightPage(dateRightPage + DAYS_TO_CHANGE_BY * MILLISECONDS_PER_DAY);
+  }
+
+  function handleLeftDatePick(selectedDate) {
+    setDateLeftPage(selectedDate.getTime());
+    setDateRightPage(selectedDate.getTime() + DAYS_TO_CHANGE_BY * MILLISECONDS_PER_DAY);
+  }
+
+  function handleRightDatePick(selectedDate) {
+    setDateRightPage(selectedDate.getTime());
+    setDateLeftPage(selectedDate.getTime() - DAYS_TO_CHANGE_BY * MILLISECONDS_PER_DAY);
+  }
+
+  return (
+    <div className="Journal">
+      <div className="Half-journal">
+        <ArrowBackIcon className="Arrow-left" onClick={handleLeftNav} />
+        <DatePicker
+          selected={dateLeftPage}
+          onChange={(selectedDate) => handleLeftDatePick(selectedDate)}
+          dateFormat="MMMM d, yyyy"
+          className="Date-select"
+        />
+        <Page newDate={dateLeftPage} className="Page" />
+      </div>
+      <div className="Half-journal">
+        <DatePicker
+          selected={dateRightPage}
+          onChange={(selectedDate) => handleRightDatePick(selectedDate)}
+          dateFormat="MMMM d, yyyy"
+          className="Date-select"
+        />
+        <Page newDate={dateRightPage} className="Page" />
+        <ArrowForwardIcon className="Arrow-right" onClick={handleRightNav} />
+      </div>
+    </div>
+  );
+};
 
 export default Journal;
