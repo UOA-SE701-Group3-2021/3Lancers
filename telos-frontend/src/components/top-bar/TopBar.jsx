8816ba886b5bf897471488bb02c31677e010e0a5
RawiriHohepa <54158903+RawiriHohepa@users.noreply.github.com>
2021-03-20 15:32:15 +1300

    Created Menu, updated routes for dashboard widgets

--- telos-frontend/src/components/top-bar/TopBar.jsx
+++ telos-frontend/src/components/top-bar/TopBar.jsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
+import Menu from '@material-ui/core/Menu';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -6,6 +9,12 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import SettingsIcon from '@material-ui/icons/Settings';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import ImportContactsIcon from '@material-ui/icons/ImportContacts';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import CancelIcon from '@material-ui/icons/Cancel';
 
 //  Used to define styles of MUI components.
 const useStyles = makeStyles(() => ({
@@ -25,9 +34,22 @@ const useStyles = makeStyles(() => ({
 const TopBar = () => {
   const classes = useStyles();
 
-  //   Stub for menu button handler
-  function menuHandler() {
-    console.log('Menu button clicked');
+  const [anchorEl, setAnchorEl] = useState(null);
+  const history = useHistory();
+
+  function menuOpenHandler(event) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function menuCloseHandler() {
+    setAnchorEl(null);
+  }
+
+  function menuChoiceHandler(widget) {
+    if (widget) {
+      history.push(`/${widget}`);
+    }
+    menuCloseHandler();
   }
 
   //   Stub for user button handler
@@ -44,7 +66,7 @@ const TopBar = () => {
     <div>
       <AppBar position="static" className={classes.root}>
         <Toolbar>
-          <IconButton onClick={menuHandler} edge="start" color="inherit" aria-label="menu">
+          <IconButton onClick={menuOpenHandler} edge="start" color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
 
@@ -65,6 +87,35 @@ const TopBar = () => {
           </section>
         </Toolbar>
       </AppBar>
+
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={menuCloseHandler}
+      >
+        <MenuItem onClick={() => menuChoiceHandler('journal')}>
+          <ImportContactsIcon />
+          Journal
+        </MenuItem>
+        <MenuItem onClick={() => menuChoiceHandler('calendar')}>
+          <CalendarTodayIcon />
+          Calendar
+        </MenuItem>
+        <MenuItem onClick={() => menuChoiceHandler('todo')}>
+          <CheckBoxIcon />
+          Todo List
+        </MenuItem>
+        <MenuItem onClick={() => menuChoiceHandler('habittracker')}>
+          <MoreHorizIcon />
+          Habit Tracker
+        </MenuItem>
+        <MenuItem onClick={menuCloseHandler}>
+          <CancelIcon />
+          Cancel
+        </MenuItem>
+      </Menu>
     </div>
   );
 };
