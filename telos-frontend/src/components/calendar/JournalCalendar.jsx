1ce6265da8d2e48e55d43e30572bed7e03771bfd
maheknangia <63040474+maheknangia@users.noreply.github.com>
2021-03-21 12:59:44 +1300

    refactored file

--- telos-frontend/src/components/calendar/JournalCalendar.jsx
+++ telos-frontend/src/components/calendar/JournalCalendar.jsx
@@ -1,10 +1,6 @@
-import * as React from "react";
-import Paper from "@material-ui/core/Paper";
-import {
-  ViewState,
-  EditingState,
-  IntegratedEditing
-} from "@devexpress/dx-react-scheduler";
+import * as React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
   DayView,
@@ -12,85 +8,85 @@ import {
   Resources,
   EditRecurrenceMenu,
   AppointmentForm,
-  AppointmentTooltip
-} from "@devexpress/dx-react-scheduler-material-ui";
+  AppointmentTooltip,
+} from '@devexpress/dx-react-scheduler-material-ui';
 
 const calendarData = [
   {
     priorityId: 2,
     typeId: 1,
-    title: "Coffee",
-    startDate: new Date("2021-03-21T20:00:00.000Z"),
-    endDate: new Date("2021-03-21T20:30:00.000Z")
+    title: 'Coffee',
+    startDate: new Date('2021-03-21T20:00:00.000Z'),
+    endDate: new Date('2021-03-21T20:30:00.000Z'),
   },
   {
     priorityId: 2,
     typeId: 2,
-    title: "SOFTENG 754",
-    startDate: new Date("2021-03-21T21:00:00.000Z"),
-    endDate: new Date("2021-03-21T23:00:00.000Z")
+    title: 'SOFTENG 754',
+    startDate: new Date('2021-03-21T21:00:00.000Z'),
+    endDate: new Date('2021-03-21T23:00:00.000Z'),
   },
   {
     priorityId: 2,
     typeId: 2,
-    title: "SOFTENG 701",
-    startDate: new Date("2021-03-21T23:00:00.000Z"),
-    endDate: new Date("2021-03-22T01:00:00.000Z")
+    title: 'SOFTENG 701',
+    startDate: new Date('2021-03-21T23:00:00.000Z'),
+    endDate: new Date('2021-03-22T01:00:00.000Z'),
   },
   {
-    priorityId:1,
+    priorityId: 1,
     typeId: 1,
-    title: "Lunch",
-    startDate: new Date("2021-03-22T01:00:00.000Z"),
-    endDate: new Date("2021-03-22T02:30:00.000Z")
+    title: 'Lunch',
+    startDate: new Date('2021-03-22T01:00:00.000Z'),
+    endDate: new Date('2021-03-22T02:30:00.000Z'),
   },
   {
     priorityId: 1,
     typeId: 2,
-    title: "701 Meeting",
-    startDate: new Date("2021-03-22T03:30:00.000Z"),
-    endDate: new Date("2021-03-22T04:30:00.000Z")
+    title: '701 Meeting',
+    startDate: new Date('2021-03-22T03:30:00.000Z'),
+    endDate: new Date('2021-03-22T04:30:00.000Z'),
   },
   {
     priorityId: 1,
     typeId: 2,
-    title: "754 Meeting",
-    startDate: new Date("2021-03-22T04:30:00.000Z"),
-    endDate: new Date("2021-03-22T05:30:00.000Z")
+    title: '754 Meeting',
+    startDate: new Date('2021-03-22T04:30:00.000Z'),
+    endDate: new Date('2021-03-22T05:30:00.000Z'),
   },
   {
     priorityId: 2,
     typeId: 2,
-    title: "Work",
-    startDate: new Date("2021-03-22T06:00:00.000Z"),
-    endDate: new Date("2021-03-22T08:00:00.000Z")
-  }
+    title: 'Work',
+    startDate: new Date('2021-03-22T06:00:00.000Z'),
+    endDate: new Date('2021-03-22T08:00:00.000Z'),
+  },
 ];
 
 export const priorityData = [
   {
-    text: "Low Priority",
+    text: 'Low Priority',
     id: 1,
-    color: "#D6ADFF"
+    color: '#D6ADFF',
   },
   {
-    text: "High Priority",
+    text: 'High Priority',
     id: 2,
-    color: "#7A00F4"
-  }
+    color: '#7A00F4',
+  },
 ];
 
 export const typeData = [
   {
-    text: "Home",
+    text: 'Home',
     id: 1,
-    color: "#ADADFF"
+    color: '#ADADFF',
   },
   {
-    text: "University",
+    text: 'University',
     id: 2,
-    color: "#0000de"
-  }
+    color: '#0000de',
+  },
 ];
 export default class Calendar extends React.PureComponent {
   constructor(props) {
@@ -99,18 +95,18 @@ export default class Calendar extends React.PureComponent {
       data: calendarData,
       resources: [
         {
-          fieldName: "priorityId",
-          title: "Priority",
+          fieldName: 'priorityId',
+          title: 'Priority',
           instances: priorityData,
-          allowMultiple: false
+          allowMultiple: false,
         },
         {
-          fieldName: "typeId",
-          title: "Type",
+          fieldName: 'typeId',
+          title: 'Type',
           instances: typeData,
-          allowMultiple: false
-        }
-      ]
+          allowMultiple: false,
+        },
+      ],
     };
 
     this.commitChanges = this.commitChanges.bind(this);
@@ -120,15 +116,12 @@ export default class Calendar extends React.PureComponent {
     this.setState((state) => {
       let { data } = state;
       if (added) {
-        const startingAddedId =
-            data.length > 0 ? data[data.length - 1].id + 1 : 0;
+        const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
         data = [...data, { id: startingAddedId, ...added }];
       }
       if (changed) {
         data = data.map((appointment) =>
-            changed[appointment.id]
-                ? { ...appointment, ...changed[appointment.id] }
-                : appointment
+          changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment
         );
       }
       if (deleted !== undefined) {
@@ -142,19 +135,19 @@ export default class Calendar extends React.PureComponent {
     const { data, resources } = this.state;
 
     return (
-        <Paper>
-          <Scheduler data={data}>
-            <ViewState defaultCurrentDate="2021-03-22" />
-            <EditingState onCommitChanges={this.commitChanges} />
-            <IntegratedEditing />
-            <EditRecurrenceMenu />
-            <DayView startDayHour={0} endDayHour={24} />
-            <Appointments />
-            <AppointmentTooltip showOpenButton showDeleteButton showCloseButton />
-            <AppointmentForm />
-            <Resources data={resources} mainResourceName="priorityId" />
-          </Scheduler>
-        </Paper>
+      <Paper>
+        <Scheduler data={data}>
+          <ViewState defaultCurrentDate="2021-03-22" />
+          <EditingState onCommitChanges={this.commitChanges} />
+          <IntegratedEditing />
+          <EditRecurrenceMenu />
+          <DayView startDayHour={0} endDayHour={24} />
+          <Appointments />
+          <AppointmentTooltip showOpenButton showDeleteButton showCloseButton />
+          <AppointmentForm />
+          <Resources data={resources} mainResourceName="priorityId" />
+        </Scheduler>
+      </Paper>
     );
   }
 }
