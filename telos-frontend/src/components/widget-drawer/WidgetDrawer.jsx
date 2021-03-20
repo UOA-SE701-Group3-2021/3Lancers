98b295a49edf293a24cb2d55d9cd1038e6b92157
Benjamin-Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2021-03-20 18:17:08 +1300

    Did prettier fix

--- telos-frontend/src/components/widget-drawer/WidgetDrawer.jsx
+++ telos-frontend/src/components/widget-drawer/WidgetDrawer.jsx
@@ -1,13 +1,15 @@
 import './WidgetDrawer.css';
 import { useState } from 'react';
 
-const Arrow = () =>
+const Arrow = () => (
   <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-    <path d="M0 0h24v24H0V0z" fill="none" opacity=".87" /><path d="M17.51 3.87L15.73 2.1 5.84 12l9.9 9.9 1.77-1.77L9.38 12l8.13-8.13z" />
+    <path d="M0 0h24v24H0V0z" fill="none" opacity=".87" />
+    <path d="M17.51 3.87L15.73 2.1 5.84 12l9.9 9.9 1.77-1.77L9.38 12l8.13-8.13z" />
   </svg>
+);
 
 const WidgetDrawer = ({ children }) => {
-
+  const BLANK = '';
   const [isHidden, setisHidden] = useState(false);
 
   function handleOnClick() {
@@ -16,14 +18,12 @@ const WidgetDrawer = ({ children }) => {
 
   return (
     <div className="WidgetDrawerParent">
-      <div className={`closer ${isHidden ? "flipped" : ""}`} onClick={handleOnClick}>
+      <div className={`closer ${isHidden ? 'flipped' : BLANK}`} onClick={handleOnClick}>
         <Arrow />
       </div>
-      <div className={`WidgetDrawer ${isHidden ? "hidden" : ""}`}>
-        {children}
-      </div>
+      <div className={`WidgetDrawer ${isHidden ? 'hidden' : BLANK}`}>{children}</div>
     </div>
   );
-}
+};
 
 export default WidgetDrawer;
