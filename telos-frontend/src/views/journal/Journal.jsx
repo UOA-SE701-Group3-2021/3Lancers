84789012832300ce022f0bd6c6b29fa7687dd6ad
Andy Huang <49605656+ambiyu@users.noreply.github.com>
2021-03-21 19:59:24 +1300

    Implemented page specific widgets (#181)

--- telos-frontend/src/views/journal/Journal.jsx
+++ telos-frontend/src/views/journal/Journal.jsx
@@ -1,22 +1,19 @@
-import { useState, useCallback } from 'react';
-import { useDrop } from 'react-dnd';
-import update from 'immutability-helper';
+import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import DraggableWidget from '../../dnd/DraggableWidget';
-import { WidgetTypes } from '../../dnd/WidgetTypes';
 import Page from './Page';
 import './Journal.css';
 import 'react-datepicker/dist/react-datepicker.css';
 
+const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+const DAYS_TO_CHANGE_BY = 1;
+
 const Journal = () => {
-  const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
-  const DAYS_TO_CHANGE_BY = 1;
-  const date = new Date();
-  const [dateLeftPage, setDateLeftPage] = useState(date.getTime());
+  const now = new Date();
+  const [dateLeftPage, setDateLeftPage] = useState(now.getTime());
   const [dateRightPage, setDateRightPage] = useState(
-    date.getTime() + DAYS_TO_CHANGE_BY * MILLISECONDS_PER_DAY
+    now.getTime() + DAYS_TO_CHANGE_BY * MILLISECONDS_PER_DAY
   );
 
   function handleLeftNav() {
@@ -39,52 +36,13 @@ const Journal = () => {
     setDateLeftPage(selectedDate.getTime() - DAYS_TO_CHANGE_BY * MILLISECONDS_PER_DAY);
   }
 
-  const [widgets, setWidgets] = useState([
-    { widgetType: 'todo', id: 0, top: 0, left: 0 },
-    { widgetType: 'habit_tracker', id: 1, top: 200, left: 200 },
-    { widgetType: 'text', id: 2, top: 100, left: 100 },
-    { widgetType: 'calendar', id: 3, top: 300, left: 300 },
-  ]);
-
-  const moveWidget = useCallback(
-    (id, left, top) => {
-      setWidgets(
-        update(widgets, {
-          [id]: {
-            $merge: { left, top },
-          },
-        })
-      );
-    },
-    [widgets]
-  );
-
-  const [, drop] = useDrop(
-    () => ({
-      accept: [
-        WidgetTypes.TODO_LIST,
-        WidgetTypes.CALENDAR,
-        WidgetTypes.HABIT_TRACKER,
-        WidgetTypes.TEXT,
-      ],
-      drop(item, monitor) {
-        const delta = monitor.getDifferenceFromInitialOffset();
-
-        const left = Math.round(item.left + delta.x);
-        const top = Math.round(item.top + delta.y);
-
-        moveWidget(item.id, left, top);
-        return undefined;
-      },
-    }),
-    [moveWidget]
-  );
+  function formatDateString(dateMilliseconds) {
+    const date = new Date(dateMilliseconds);
+    return new Date(date.getTime() - date.getTimezoneOffset() * 60000).toISOString().split('T')[0];
+  }
 
   return (
-    <div className="Journal" ref={drop}>
-      {widgets.map((widget) => (
-        <DraggableWidget {...widget} />
-      ))}
+    <div className="Journal">
       <div className="Half-journal">
         <ArrowBackIcon className="Arrow-left" onClick={handleLeftNav} />
         <DatePicker
@@ -93,7 +51,7 @@ const Journal = () => {
           dateFormat="MMMM d, yyyy"
           className="Date-select"
         />
-        <Page newDate={dateLeftPage} className="Page" />
+        <Page date={formatDateString(dateLeftPage)} leftPage />
       </div>
       <div className="Half-journal">
         <DatePicker
@@ -102,7 +60,7 @@ const Journal = () => {
           dateFormat="MMMM d, yyyy"
           className="Date-select"
         />
-        <Page newDate={dateRightPage} className="Page" />
+        <Page date={formatDateString(dateRightPage)} />
         <ArrowForwardIcon className="Arrow-right" onClick={handleRightNav} />
       </div>
     </div>
