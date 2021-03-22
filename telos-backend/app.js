cff6acd29208a9b60e03ccd047e97bcccf9abfed
Felix Yang <49087744+flexzy@users.noreply.github.com>
2021-03-23 01:35:44 +1300

    Add cors header (#194)
    * Added isOverdue field into the todo response data structure
    
    * Added cors to all responses from API
    
    * Fixed linting errors

--- telos-backend/app.js
+++ telos-backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 // Setup Express and assign it a port (cannot be same as CRA default port)
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
