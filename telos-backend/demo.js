96796c7428b52caf0a7974164e99604d589dba39
Caelan Murch <werer9@gmail.com>
2021-03-24 23:47:20 +1300

    created lorem ipsum text

--- telos-backend/demo.js
+++ telos-backend/demo.js
@@ -1,15 +1,38 @@
 const TodoTask = require('./src/models/todo_task');
 
+// Check if there are any TodoTask documents
 TodoTask.find().countDocuments((err, count) => {
+  // If there are none
   if (count === 0) {
-    console.log('Empty');
+    // create new TodoTask document
     const entry = new TodoTask({
       name: 'Lorem',
       createdDate: '2021-01-01',
       dueDate: '2021-01-01',
       completed: false,
     });
+    // save document
+    entry.save();
+  }
+});
+
+const Widget = require('./src/models/widget');
+const Text = require('./src/models/text');
+
+Text.find().countDocuments((err, count) => {
+  if (count === 0) {
+    console.log('Empty');
+    const entry = new Text({
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
+            'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' + 
+            'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'  + 
+            'ut aliquip ex ea commodo consequat. Duis aute irure dolor in' +
+            'reprehenderit in voluptate velit esse cillum dolore eu fugiat' + 
+            'nulla pariatur. Excepteur sint occaecat cupidatat non proident,' + 
+            'sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    });
 
     entry.save();
   }
 });
+
