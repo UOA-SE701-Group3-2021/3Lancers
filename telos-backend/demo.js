4cebfa37a4b7f4a0ba639a66a622b97e7e712f46
Caelan Murch <werer9@gmail.com>
2021-03-25 00:10:57 +1300

    working dummy text generation

--- telos-backend/demo.js
+++ telos-backend/demo.js
@@ -16,27 +16,21 @@ TodoTask.find().countDocuments((err, count) => {
   }
 });
 
-const Widget = require('./src/models/widget');
 const Text = require('./src/models/text');
 
 // look for existing text documents
 Text.find().countDocuments((err, count) => {
-  // If there are no text documents
-  if (count === 0) {
-    console.log('Empty');
-    // create a new dummy document
-    const entry = new Text({
-      widgetId: 'abcdee6a0ba62570afcedd3a',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-            'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' + 
-            'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'  + 
-            'ut aliquip ex ea commodo consequat. Duis aute irure dolor in' +
-            'reprehenderit in voluptate velit esse cillum dolore eu fugiat' + 
-            'nulla pariatur. Excepteur sint occaecat cupidatat non proident,' + 
-            'sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    });
-    // save text document
-    entry.save();
-  }
+  const entry = new Text({
+    widgetId: 'abcdee6a0ba62570afcedd3a',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
+          'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' + 
+          'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'  + 
+          'ut aliquip ex ea commodo consequat. Duis aute irure dolor in' +
+          'reprehenderit in voluptate velit esse cillum dolore eu fugiat' + 
+          'nulla pariatur. Excepteur sint occaecat cupidatat non proident,' + 
+          'sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  });
+  // save text document
+  entry.save();
 });
 
