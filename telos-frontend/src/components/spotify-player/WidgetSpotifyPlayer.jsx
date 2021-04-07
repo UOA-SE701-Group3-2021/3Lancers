ebc3cf4f8080927c2f83c87dc1425f62b62bdb71
ben <bche536@aucklanduni.ac.nz>
2021-04-07 16:14:44 +1200

    added logo to music side widget

--- telos-frontend/src/components/spotify-player/WidgetSpotifyPlayer.jsx
+++ telos-frontend/src/components/spotify-player/WidgetSpotifyPlayer.jsx
@@ -1,8 +1,10 @@
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
 import styles from '../Widget.module.css';
+import './WidgetSpotifyPlayer.module.css';
 
 const WidgetSpotifyPlayer = ({ addNewSpotifyPlayer }) => (
   <div onClick={addNewSpotifyPlayer} className={`${styles.widgetIcon} ${styles.vertical}`}>
-    <div className={styles.bigText}>S</div>
+    <MusicNoteIcon style={{ fontSize: 35 }} />
   </div>
 );
 
