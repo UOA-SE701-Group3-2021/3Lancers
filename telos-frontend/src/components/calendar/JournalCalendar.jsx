ff15e0a4781a9ddf8020c526425dba1edeec10ef
Wong <wcho400@aucklanduni.ac.nz>
2021-04-07 15:46:40 +1200

    Refactored JournalCalendar class to same format

--- telos-frontend/src/components/calendar/JournalCalendar.jsx
+++ telos-frontend/src/components/calendar/JournalCalendar.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
 import { FaTimes } from 'react-icons/fa';
@@ -95,34 +95,29 @@ export const typeData = [
 ];
 
 // resources allow to differentiate between the different types of calendar options
-export default class Calendar extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: calendarData,
-      resources: [
-        {
-          fieldName: 'priorityId',
-          title: 'Priority',
-          instances: priorityData,
-          allowMultiple: false,
-        },
-        {
-          fieldName: 'typeId',
-          title: 'Type',
-          instances: typeData,
-          allowMultiple: false,
-        },
-      ],
-    };
 
-    this.commitChanges = this.commitChanges.bind(this);
-  }
+const JournalCalendar = ({ id, deleteWidget }) => {
+  const [state, setState] = useState({
+    data: calendarData,
+    resources: [
+      {
+        fieldName: 'priorityId',
+        title: 'Priority',
+        instances: priorityData,
+        allowMultiple: false,
+      },
+      {
+        fieldName: 'typeId',
+        title: 'Type',
+        instances: typeData,
+        allowMultiple: false,
+      },
+    ],
+  });
 
-  // depending on the state, carry out the specific action
-  commitChanges({ added, changed, deleted }) {
-    this.setState((state) => {
-      let { data } = state;
+  function commitChanges({ added, changed, deleted }) {
+    setState((newState) => {
+      let { data } = newState;
       if (added) {
         const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
         data = [...data, { id: startingAddedId, ...added }];
@@ -139,29 +134,98 @@ export default class Calendar extends React.PureComponent {
     });
   }
 
-  render() {
-    const { data, resources } = this.state;
+  return (
+    <>
+      <Paper style={{ height: '100%' }}>
+        <div className={styles.header}>
+          <FaTimes className={styles.cross} onClick={() => deleteWidget(id)} />
+        </div>
 
-    return (
-      <>
-        <Paper style={{ height: '100%' }}>
-          <div className={styles.header}>
-            <FaTimes className={styles.cross} onClick={() => deleteWidget(id)} />{' '}
-          </div>
+        <Scheduler data={state.data}>
+          <ViewState defaultCurrentDate="2021-03-22" />
+          <EditingState onCommitChanges={commitChanges} />
+          <IntegratedEditing />
+          <EditRecurrenceMenu />
+          <DayView startDayHour={0} endDayHour={24} />
+          <Appointments />
+          <AppointmentTooltip showOpenButton showDeleteButton showCloseButton />
+          <AppointmentForm />
+          <Resources data={state.resources} mainResourceName="priorityId" />
+        </Scheduler>
+      </Paper>
+    </>
+  );
+};
 
-          <Scheduler data={data}>
-            <ViewState defaultCurrentDate="2021-03-22" />
-            <EditingState onCommitChanges={this.commitChanges} />
-            <IntegratedEditing />
-            <EditRecurrenceMenu />
-            <DayView startDayHour={0} endDayHour={24} />
-            <Appointments />
-            <AppointmentTooltip showOpenButton showDeleteButton showCloseButton />
-            <AppointmentForm />
-            <Resources data={resources} mainResourceName="priorityId" />
-          </Scheduler>
-        </Paper>
-      </>
-    );
-  }
-}
+export default JournalCalendar;
+
+// export default class Calendar extends React.PureComponent {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: calendarData,
+//       resources: [
+//         {
+//           fieldName: 'priorityId',
+//           title: 'Priority',
+//           instances: priorityData,
+//           allowMultiple: false,
+//         },
+//         {
+//           fieldName: 'typeId',
+//           title: 'Type',
+//           instances: typeData,
+//           allowMultiple: false,
+//         },
+//       ],
+//     };
+
+//     this.commitChanges = this.commitChanges.bind(this);
+//   }
+
+//   // depending on the state, carry out the specific action
+//   // commitChanges({ added, changed, deleted }) {
+//   //   this.setState((state) => {
+//   //     let { data } = state;
+//   //     if (added) {
+//   //       const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
+//   //       data = [...data, { id: startingAddedId, ...added }];
+//   //     }
+//   //     if (changed) {
+//   //       data = data.map((appointment) =>
+//   //         changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment
+//   //       );
+//   //     }
+//   //     if (deleted !== undefined) {
+//   //       data = data.filter((appointment) => appointment.id !== deleted);
+//   //     }
+//   //     return { data };
+//   //   });
+//   // }
+
+//   render() {
+//     const { data, resources } = this.state;
+
+//     // return (
+//     //   <>
+//     //     <Paper style={{ height: '100%' }}>
+//     //       <div className={styles.header}>
+//     //         <FaTimes className={styles.cross} />
+//     //       </div>
+
+//     //       <Scheduler data={data}>
+//     //         <ViewState defaultCurrentDate="2021-03-22" />
+//     //         <EditingState onCommitChanges={this.commitChanges} />
+//     //         <IntegratedEditing />
+//     //         <EditRecurrenceMenu />
+//     //         <DayView startDayHour={0} endDayHour={24} />
+//     //         <Appointments />
+//     //         <AppointmentTooltip showOpenButton showDeleteButton showCloseButton />
+//     //         <AppointmentForm />
+//     //         <Resources data={resources} mainResourceName="priorityId" />
+//     //       </Scheduler>
+//     //     </Paper>
+//     //   </>
+//     // );
+//   }
+// }
