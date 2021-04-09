0ac65490459b4bc903a58150d11832d0265c39b7
ben <bche536@aucklanduni.ac.nz>
2021-04-09 19:11:16 +1200

    added autosearch function and default youtube video id

--- telos-frontend/src/components/youtube-player/JournalYoutubePlayer.jsx
+++ telos-frontend/src/components/youtube-player/JournalYoutubePlayer.jsx
@@ -2,32 +2,78 @@ import { useState } from 'react';
 import { FaTimes } from 'react-icons/fa';
 import styles from './JournalYoutubePlayer.module.css';
 
+function YouTubeGetID(urlParam) {
+  let ID = '';
+  let url = urlParam;
+  url = url.replace(/(>|<)/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+  if (url[2] !== undefined) {
+    ID = url[2].split(/[^0-9a-z_\\-]/i);
+    [ID] = ID;
+  } else {
+    ID = url;
+  }
+  return ID;
+}
+
+// // function YoutubeWidget({ id, deleteWidget }) {
+// //   const [vidID, setVidID] = useState('');
+// //   const [displayFrame, setDisplayFrame] = useState(false);
+
+// //   useEffect(() => {
+// //     setVidID('DWcJFNfaw9c');
+// //   });
+
+// //   return (
+// //     <div className={styles.ytWidget}>
+// //       <div className={styles.header}>
+// //         <FaTimes className={styles.cross} onClick={() => deleteWidget(id)} />
+// //       </div>
+// //       {displayFrame ? (
+// //         <iframe
+// //           id="ytplayer"
+// //           title="youtube"
+// //           type="text/html"
+// //           width="720"
+// //           height="405"
+// //           src={`https://www.youtube.com/embed/${vidID}`}
+// //           frameBorder="0"
+// //           allowFullScreen
+// //         />
+// //       ) : (
+// //         <button onClick={() => setDisplayFrame(true)} type="submit" className={styles.submitBtn}>
+// //           Submit
+// //         </button>
+// //       )}
+// //       <input
+// //         className={styles.inputField}
+// //         onChange={(e) => setVidID(YouTubeGetID(e.target.value))}
+// //       />
+// //     </div>
+// //   );
+// // }
+
 function YoutubeWidget({ id, deleteWidget }) {
-  const [vidID, setVidID] = useState('0');
-  const [displayFrame, setDisplayFrame] = useState(false);
+  const [vidID, setVidID] = useState('5qap5aO4i9A');
 
   return (
     <div className={styles.ytWidget}>
       <div className={styles.header}>
         <FaTimes className={styles.cross} onClick={() => deleteWidget(id)} />
       </div>
-      {displayFrame ? (
-        <iframe
-          id="ytplayer"
-          title="youtube"
-          type="text/html"
-          width="720"
-          height="405"
-          src={`https://www.youtube.com/embed/${vidID}`}
-          frameBorder="0"
-          allowFullScreen
-        />
-      ) : (
-        <button onClick={() => setDisplayFrame(true)} type="submit" className={styles.submitBtn}>
-          Submit
-        </button>
-      )}
-      <input className={styles.inputField} onChange={(e) => setVidID(e.target.value)} />
+      <iframe
+        id="ytplayer"
+        title="youtube"
+        type="text/html"
+        width="auto"
+        height="auto"
+        src={`https://www.youtube.com/embed/${vidID}`}
+        frameBorder="0"
+        allowFullScreen
+      />
+      <input
+        className={styles.inputField}
+        onChange={(e) => setVidID(YouTubeGetID(e.target.value))}
+      />
     </div>
   );
 }
