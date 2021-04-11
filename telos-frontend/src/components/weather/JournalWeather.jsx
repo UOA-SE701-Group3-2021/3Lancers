9b90e18b068a77238a6e77ac5e70dc66275d38af
Sasindu Jayasinghe <sasindu.jayasinghe@gmail.com>
2021-04-11 18:07:25 +1200

    Update JournalWeather.jsx

--- telos-frontend/src/components/weather/JournalWeather.jsx
+++ telos-frontend/src/components/weather/JournalWeather.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import { FaTimes } from 'react-icons/fa';
 import styles from './JournalWeather.module.css'
 
