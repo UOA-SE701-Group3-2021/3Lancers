e1012f039bc16aa40d95a5639ea43a4fbfc53498
OverCry <54381704+OverCry@users.noreply.github.com>
2021-04-11 23:39:45 +1200

    Refactoring widgets and css (#279)
    * renamed component import to make more sense
    
    * Refactored deleting header to be more reusable
    
    * Fixed text widget spacing issue
    
    * Change text widget width
    
    * Removed double headers
    
    * temp fix for clock size
    
    * modified header

--- telos-frontend/src/components/clock/Clock.jsx
+++ telos-frontend/src/components/clock/Clock.jsx
@@ -1,10 +1,9 @@
 import Box from '@material-ui/core/Box';
 import { useState, useEffect } from 'react';
-import { FaTimes } from 'react-icons/fa';
 import styles from './WidgetClock.module.css';
 
 // This react component displayed the current time
-const Clock = ({ id, deleteWidget }) => {
+const Clock = () => {
   const [clockContent, setClockContent] = useState('');
 
   useEffect(() => {
@@ -19,10 +18,6 @@ const Clock = ({ id, deleteWidget }) => {
   return (
     <>
       <Box className={styles.box}>
-        <div className={styles.header}>
-          {' '}
-          <FaTimes className={styles.cross} onClick={() => deleteWidget(id)} />
-        </div>
         <div className={styles.clockContent}>{clockContent}</div>
       </Box>
     </>
