c1e112e68de86cd2f32dafbb77ac339e9783995b
Alex Verkerk <44216323+Alxjlv@users.noreply.github.com>
2021-04-12 21:46:24 +1200

    Added backend Steam API owned games data collection

--- telos-backend/src/routes/api/steam.js
+++ telos-backend/src/routes/api/steam.js
@@ -6,23 +6,94 @@ const router = express.Router();
 
 const steamKey = process.env.STEAM_KEY;
 
+/**
+ * Filter the unwanted fields from the steam data & sort by playtime
+ * */
+const filterSteamData = (steamData) => {
+  // Remove the fields that are too specific
+  const trimmedData = steamData.map(
+    ({
+      // eslint-disable-next-line camelcase
+      img_logo_url,
+      // eslint-disable-next-line camelcase
+      has_community_visible_stats,
+      // eslint-disable-next-line camelcase
+      playtime_windows_forever,
+      // eslint-disable-next-line camelcase
+      playtime_mac_forever,
+      // eslint-disable-next-line camelcase
+      playtime_linux_forever,
+      ...keepAttrs
+    }) => keepAttrs
+  );
+  const newData = trimmedData.map((item) => {
+    const { appid: id, ...rest } = item;
+    return { id, ...rest };
+  });
+  // Sort by playtime first and then alphabetically
+  newData.sort((game1, game2) =>
+    // eslint-disable-next-line no-nested-ternary
+    game1.playtime_forever < game2.playtime_forever
+      ? 1
+      : // eslint-disable-next-line no-nested-ternary
+      game1.playtime_forever === game2.playtime_forever
+      ? game1.name > game2.name
+        ? 1
+        : -1
+      : -1
+  );
+  return newData;
+};
+
+/**
+ * GET /?steamVanity=<vanityURL> endpoint to get the game library for a user
+ * Returns: 500 if the environmental variable cannot be found or if the server breaks.
+ *          404 if some of the steam data cannot be retrieved.
+ *          200 if the data retrieval request was successful
+ */
 router.get('/', async (req, res) => {
-  console.log(steamKey);
+  let steamId;
+  // Checking if the STEAM_KEY environmental variable has been found
   if (!steamKey) {
     return res.status(500).json({ error: 'No Key found in environment' });
   }
+  // Retrieve vanity URL from query params
   const { steamVanity } = req.query;
+  if (!steamVanity) {
+    return res
+      .status(400)
+      .json({ error: 'Malformed request: steamVanity query parameter is invalid' });
+  }
+  // TODO - pass in Steamworks URL for dependency injection
   await axios
     .get(
       `https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key=${steamKey}&vanityurl=${steamVanity}`
     )
     .then((response) => {
+      // Success of 1 from SteamWorks indicates a successful request
       if (response.data.response.success === 1) {
-        console.log(response.data.response.steamid);
-        return res.status(200).json(response.data.response.steamid);
+        steamId = response.data.response.steamid;
+        return true;
       }
       return res.status(404).json({ error: 'No match for that username' });
     });
+  if (steamId) {
+    console.log(`Steam ID ${steamId} found for ${steamVanity}`);
+    await axios
+      .get(
+        `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${steamKey}&steamid=${steamId}&include_appinfo=true`
+      )
+      .then((response) => {
+        // Checking to see if any games were returned
+        if (response.data.response.game_count > 0) {
+          console.log(`Found ${response.data.response.game_count} game(s)`);
+          return res.status(200).json(filterSteamData(response.data.response.games));
+        }
+        return res
+          .status(404)
+          .json({ error: 'No games found for that username; Their profile may be private' });
+      });
+  }
   return true;
 });
 
--- telos-frontend/src/components/steam/steam.jsx
+++ telos-frontend/src/components/steam/steam.jsx
@@ -1,42 +1,30 @@
-// import { DataGrid } from '@material-ui/data-grid';
+import { DataGrid } from '@material-ui/data-grid';
 import { FaTimes } from 'react-icons/fa';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import styles from './WidgetSteam.module.css';
 
-// const steamColumns = [
-//   { field: 'appid', headerName: 'App ID' },
-//   { field: 'name', headerName: 'Name' },
-//   { field: 'playtime_forever', headerName: 'Hours played' },
-//   { field: 'img_icon_url', headerName: 'Image' },
-// ];
+const steamColumns = [
+  { field: 'appid', headerName: 'App ID' },
+  { field: 'name', headerName: 'Name' },
+  { field: 'playtime_forever', headerName: 'Hours played' },
+  { field: 'img_icon_url', headerName: 'Image' },
+];
 
 const Steam = ({ id, deleteWidget }) => {
   const [loadingData, setLoadingData] = useState(true);
   const [data, setData] = useState();
+  const [dataMessage, setDataMessage] = useState('Loading steam data...');
   useEffect(() => {
     const getSteamData = async (userId) => {
       await axios.get(`/api/steam?steamVanity=${userId}`).then((response) => {
-        if (response.data) {
-          // setData(
-          //   response.games.map(
-          //     ({
-          //       // eslint-disable-next-line camelcase
-          //       img_logo_url,
-          //       // eslint-disable-next-line camelcase
-          //       has_community_visible_stats,
-          //       // eslint-disable-next-line camelcase
-          //       playtime_windows_forever,
-          //       // eslint-disable-next-line camelcase
-          //       playtime_mac_forever,
-          //       // eslint-disable-next-line camelcase
-          //       playtime_linux_forever,
-          //       ...keepAttrs
-          //     }) => keepAttrs
-          //   )
-          // );
-          setData(response.data);
-          setLoadingData(false);
+        if (response.status === 200) {
+          if (response.data) {
+            setData(response.data);
+            setLoadingData(false);
+          }
+        } else {
+          setDataMessage('Could not retrieve steam data');
         }
       });
     };
@@ -49,10 +37,9 @@ const Steam = ({ id, deleteWidget }) => {
     <div style={{ height: 400, width: '100%' }}>
       <FaTimes className={styles.cross} onClick={() => deleteWidget(id)} />
       {loadingData ? (
-        <p>Loading steam data...</p>
+        <p>{dataMessage}</p>
       ) : (
-        // <DataGrid rows={data} columns={steamColumns} pageSize={5} checkboxSelection />
-        <p>{data}</p>
+        <DataGrid rows={data} columns={steamColumns} pageSize={5} checkboxSelection />
       )}
     </div>
   );
